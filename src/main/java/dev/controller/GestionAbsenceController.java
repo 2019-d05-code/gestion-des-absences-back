4634c302e2af0d21c6c821d3b067d8e4b15b8659
Nicolas <33360039+NicoSimplon@users.noreply.github.com>
2019-05-24 22:29:27 +0200

    Transpofmation des missions en demandes d'absences de type mission et suppression du controller /missions

--- src/main/java/dev/controller/GestionAbsenceController.java
+++ src/main/java/dev/controller/GestionAbsenceController.java
@@ -1,9 +1,16 @@
 package dev.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,8 +22,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import dev.controller.vm.DemandeAbsenceDTO;
+import dev.controller.vm.MissionDTO;
+import dev.domain.Collegue;
+import dev.repository.CollegueRepo;
 import dev.services.DemandeAbsenceService;
 
 @RestController
@@ -26,8 +38,14 @@ public class GestionAbsenceController {
 	@Autowired
 	DemandeAbsenceService service;
 	
+	@Autowired
+	CollegueRepo colRepo;
+
+	RestTemplate rt = new RestTemplate();
+
 	/**
-	 * Permet d'appeler le service checkant la validité de la demande et le cas échéant l'enregistrant
+	 * Permet d'appeler le service checkant la validité de la demande et le cas
+	 * échéant l'enregistrant
 	 * 
 	 * @param demande
 	 * @return
@@ -35,27 +53,28 @@ public class GestionAbsenceController {
 	@PostMapping
 	@Secured("ROLE_UTILISATEUR")
 	public ResponseEntity<Object> enregistrerDemandeAbsence(@RequestBody DemandeAbsenceDTO demande) {
-		
+
 		service.enregistrerDemandeAbsence(demande);
-		
+
 		return ResponseEntity.status(HttpStatus.OK).build();
-		
+
 	}
-	
+
 	/**
 	 * Récupère la liste des demandes d'absences validées d'un collègue
 	 * 
 	 * @param email
 	 * @return
 	 */
-	@GetMapping(path= "/listeAbsencesValidees")
+	@GetMapping(path = "/listeAbsencesValidees")
 	@Secured("ROLE_UTILISATEUR")
-	public ResponseEntity<List<DemandeAbsenceDTO>> afficherAbsencesValideesCollegue(@RequestParam("email") String email) {
+	public ResponseEntity<List<DemandeAbsenceDTO>> afficherAbsencesValideesCollegue(
+			@RequestParam("email") String email) {
 
 		return ResponseEntity.status(HttpStatus.OK).body(service.listeDemandesValideesParEmail(email));
 
 	}
-	
+
 	/**
 	 * Enregistre une demande de RTT employeur
 	 * 
@@ -65,26 +84,48 @@ public class GestionAbsenceController {
 	@PostMapping("/employeur-rtt")
 	@Secured("ROLE_MANAGER")
 	public ResponseEntity<Object> enregistrerDemandeRTTEmployeur(@RequestBody DemandeAbsenceDTO[] demandes) {
-		
+
 		service.enregistrementDemandeRTTEmployeur(demandes);
 		return ResponseEntity.status(HttpStatus.OK).build();
-		
+
 	}
-	
+
 	/**
-	 * Retourne la liste complète des demandes d'absences effectuées par un collègue
+	 * Retourne la liste complète des demandes d'absences effectuées par un
+	 * collègue ainsi que ses missions (considérées comme des absences)
 	 * 
 	 * @param email
 	 * @return List<DemandeAbsenceDTO>
+	 * @throws URISyntaxException 
+	 * @throws RestClientException 
 	 */
 	@GetMapping("/listeAbsences")
 	@Secured("ROLE_UTILISATEUR")
-	public ResponseEntity<List<DemandeAbsenceDTO>> recupDemandesParEmploye(@RequestParam("email") String email) {
+	public ResponseEntity<List<DemandeAbsenceDTO>> recupDemandesParEmploye(@RequestParam("email") String email, HttpServletRequest request) throws RestClientException, URISyntaxException {
+
+		String get = "https://missions-back.cleverapps.io/collegue/";
+
+		// On récupère le collègue connecté afin d'avoir ses identifiants de
+		// connexion
+		Collegue collegueConnecte = colRepo.findByEmail(email)
+				.orElseThrow(() -> new IllegalArgumentException("L'email ne correspond à aucun collegue"));
+
+		// On récupère le cookie et one le transfère dans la requête vers l'API
+		// gestion des missions
+		ResponseEntity<MissionDTO[]> respHttp2 = rt.exchange(RequestEntity.get(new URI(get + collegueConnecte.getId()))
+				.header("Cookie", request.getHeader("Cookie")).build(), MissionDTO[].class);
 		
-		return ResponseEntity.status(HttpStatus.OK).body(service.listeDemandesParEmploye(email));
+		// On récupère la liste des demandes
+		List<DemandeAbsenceDTO> listeDemandes = service.listeDemandesParEmploye(email);
 		
+		// On transforme la liste des missions récupérées en demandes et on les injecte dans la liste des demandes
+		Arrays.asList(respHttp2.getBody()).stream().map(mission -> new DemandeAbsenceDTO(mission, email)).collect(Collectors.toList())
+			.forEach(demande -> listeDemandes.add(demande));
+
+		return ResponseEntity.status(HttpStatus.OK).body(listeDemandes);
+
 	}
-	
+
 	/**
 	 * Modifie une demande d'absence
 	 * 
@@ -96,11 +137,11 @@ public class GestionAbsenceController {
 	public ResponseEntity<Object> modifierDemande(@PathVariable Long id, @RequestBody DemandeAbsenceDTO demande) {
 
 		service.modifierDemande(demande, id);
-		
+
 		return ResponseEntity.status(HttpStatus.OK).build();
-		
+
 	}
-	
+
 	/**
 	 * Supprime une demande d'absence
 	 * 
@@ -109,11 +150,11 @@ public class GestionAbsenceController {
 	@DeleteMapping("/supprimer/{id}")
 	@Secured("ROLE_UTILISATEUR")
 	public ResponseEntity<Object> supprimerDemande(@PathVariable Long id) {
-		
+
 		service.supprimerDemande(id);
-		
+
 		return ResponseEntity.status(HttpStatus.OK).build();
-		
+
 	}
-	
+
 }
